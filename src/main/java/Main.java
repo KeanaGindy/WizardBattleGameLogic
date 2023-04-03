669a870ed9865914e06ae8f5a0683460bc2239a7
Keanagindy1@gmail.com <keanagindy1@gmail.com>
2023-04-03 15:15:27 -0700

    Resolving Defect#6, utilizing constants instead of literals, fixing #7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
         System.out.println("**** GAME A ****");
         GamePlay GamePlay = new GamePlay();
-        for (int round = 1; round < 9; round++) {
+        final int TESTROUNDS=9;
+
+        for (int round = 1; round < TESTROUNDS; round++) {
             System.out.println("\tRound " + round);
             if (GamePlay.player.health > 0)
                 System.out.println("\t\tYou gained " + GamePlay.play() + " experience points during this round!!!!\n");
@@ -17,7 +19,7 @@ public class Main {
         System.out.println("**** GAME B ****");
         GamePlay gamePlay1 = new GamePlay(new Wizard());
         gamePlay1.player.health = 10;
-        for (int round = 1; round < 9; round++) {
+        for (int round = 1; round < TESTROUNDS; round++) {
             System.out.println("\tRound " + round);
             if (gamePlay1.player.health > 0)
                 System.out.println("\t\tYou gained " + gamePlay1.play() + " experience points during this round!!!!\n");
@@ -30,7 +32,7 @@ public class Main {
         System.out.println("**** GAME C ****");
         GamePlay gamePlay2 = new GamePlay(new Barbarian(), new Wizard());
         gamePlay2.Opponents.get(0).health = 5;
-        for (int round = 1; round < 9; round++) {
+        for (int round = 1; round < TESTROUNDS; round++) {
             System.out.println("\tRound " + round);
             if (gamePlay2.player.health > 0)
                 System.out.println("\t\tYou gained " + gamePlay2.play() + " experience points during this round!!!!\n");
